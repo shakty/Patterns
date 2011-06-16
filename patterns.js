b28f2a4bbcee2691dedafe3375602d1d2a662221
Michael Hellein <michael@bearded.com>
2011-06-16 09:08:25 -0400

    Issue #1011438: added patters 7 port from shakty and csko

--- patterns.js
+++ patterns.js
@@ -1,13 +1,33 @@
+(function ($) {
 
-Drupal.behaviors.patternsBehavior = function (context) {
-  $('.pattern-info').hide();
-  $('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
-  $('.pattern-title').hover(
-    function(){
-      $('#' + $(this).attr('id') + '-info').show();
-    },
-    function(){
-      $('#' + $(this).attr('id') + '-info').hide();
+  Drupal.behaviors.patternsBehavior = {
+    attach: function(context, settings) {
+	  $('.pattern-info').hide();
+	  $('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
+	  $('.pattern-title').hover(
+	    function(){
+	      $('#' + $(this).attr('id') + '-info').show();
+	    },
+	    function(){
+	      $('#' + $(this).attr('id') + '-info').hide();
+	    }
+	  );
     }
-  );
-};
+  };
+
+})(jQuery);
+
+
+
+//Drupal.behaviors.patternsBehavior = function (context) {
+//  $('.pattern-info').hide();
+//  $('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
+//  $('.pattern-title').hover(
+//    function(){
+//      $('#' + $(this).attr('id') + '-info').show();
+//    },
+//    function(){
+//      $('#' + $(this).attr('id') + '-info').hide();
+//    }
+//  );
+//};
