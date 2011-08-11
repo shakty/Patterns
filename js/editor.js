96fc3c73e483931fd0e1f485784d26ee8a69ee10
Stefano Balietti <sbalietti@ethz.ch>
2011-08-11 23:07:10 +0200

    Some modification in edit and field.inc

---
+++ js/editor.js
@@ -0,0 +1,6 @@
+window.onload = function() {
+	var editArea = document.getElementById('edit-content');
+	var myCodeMirror = CodeMirror.fromTextArea(editArea);
+};
+
+//var myCodeMirror = CodeMirror(document.body);
