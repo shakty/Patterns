bb8d718fc784ee3a6366d1201d30f6bfa9a74401
Stefano Balietti <sbalietti@ethz.ch>
2011-08-12 09:37:34 +0200

    Improved Editor

--- js/editor.js
+++ js/editor.js
@@ -1,6 +1,50 @@
 window.onload = function() {
-	var editArea = document.getElementById('edit-content');
-	var myCodeMirror = CodeMirror.fromTextArea(editArea);
+	
+	(function () {
+
+	    var editor = CodeMirror.fromTextArea(document.getElementById("edit-content"), {
+	        lineNumbers: true,
+	        onKeyEvent: function(i, e) {
+	          // Hook into F11
+	          if ((e.keyCode == 122 || e.keyCode == 27) && e.type == 'keydown') {
+	            e.stop();
+	            return toggleFullscreenEditing();
+	          }
+	        },
+	        onCursorActivity: function() {
+		        editor.setLineClass(hlLine, null);
+		        hlLine = editor.setLineClass(editor.getCursor().line, "activeline");
+		      }
+	    });
+
+	    var hlLine = editor.setLineClass(0, "activeline");
+	    
+	    
+	    var fullScreenNotice = 'Press F11 (or ESC in Safari on Mac OS X) when cursor is in the editor to toggle full screen editing.';
+	    var noticeNode = document.createTextNode(fullScreenNotice);
+	    document.getElementById("edit-content").parentNode.appendChild(noticeNode);
+	    
+	    
+	    function toggleFullscreenEditing()
+	    {
+	        var editorDiv = jQuery('.CodeMirror-scroll');
+	        if (!editorDiv.hasClass('fullscreen')) {
+	            toggleFullscreenEditing.beforeFullscreen = { height: editorDiv.height(), width: editorDiv.width() }
+	            editorDiv.addClass('fullscreen');
+	            editorDiv.height('100%');
+	            editorDiv.width('100%');
+	            editor.refresh();
+	        }
+	        else {
+	            editorDiv.removeClass('fullscreen');
+	            editorDiv.height(toggleFullscreenEditing.beforeFullscreen.height);
+	            editorDiv.width(toggleFullscreenEditing.beforeFullscreen.width);
+	            editor.refresh();
+	        }
+	    }
+
+	})();
+	
 };
 
 //var myCodeMirror = CodeMirror(document.body);
