1ae045e48b58d69bb10b473a7db8abb3f95ad475
Stefano Balietti <sbalietti@ethz.ch>
2011-08-19 12:08:03 +0200

    Small changes

--- js/ajax_validation.js
+++ js/ajax_validation.js
@@ -3,24 +3,29 @@ jQuery('document').ready( function() {
 	(function(){
 		var url = location.href.substring(0,location.href.lastIndexOf('admin/patterns/') + 'admin/patterns/'.length) + 'validate';
 		
-		document.getElementById('validate_pattern').onclick = function() {
-			document.getElementById('validation_result').innerHTML = '...validating...';
-			
-			// CO2editor is defined by editor.js (if loaded)		
-			if (typeof(CO2editor) !== "undefined") {
-				CO2editor.save();
-			}
-			
-			jQuery.ajax({
-				type: 'POST',
-				url: url,
-				data: 'pattern='+document.getElementById('edit-content').value,
-				success: function ( data, status, xhr ) {
-							document.getElementById('validation_result').innerHTML = '<strong>'+data+'</strong>';
-							//$('validation_result').replaceWith(data);
-						}
-			});
-		};
+		var textarea = document.getElementById("edit-content");
+		alert(typeof(textarea));
+		if (textarea) {
+			alert('3');
+			document.getElementById('validate_pattern').onclick = function() {
+				document.getElementById('validation_result').innerHTML = '...validating...';
+				
+				// CO2editor is defined by editor.js (if loaded)		
+				if (typeof(CO2editor) !== "undefined") {
+					CO2editor.save();
+				}
+				
+				jQuery.ajax({
+					type: 'POST',
+					url: url,
+					data: 'pattern='+document.getElementById('edit-content').value,
+					success: function ( data, status, xhr ) {
+								document.getElementById('validation_result').innerHTML = '<strong>'+data+'</strong>';
+								//$('validation_result').replaceWith(data);
+							}
+				});
+			};
+		}
 	})();
 		
 	
