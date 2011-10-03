fb8b21b926054d18f01ac05867d93fe9766d5039
Stefano Balietti <sbalietti@ethz.ch>
2011-10-03 21:20:39 +0200

    Create New Pattern and Resfresh All Button working. Closes #4 . Starting working for improved status handling in db

--- js/patterns.js
+++ js/patterns.js
@@ -1,33 +1,44 @@
-(function ($) {
+window.onload = function() {
 
-  Drupal.behaviors.patternsBehavior = {
-    attach: function(context, settings) {
-	  $('.pattern-info').hide();
-	  $('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
-	  $('.pattern-title').hover(
-	    function(){
-	      $('#' + $(this).attr('id') + '-info').show();
-	    },
-	    function(){
-	      $('#' + $(this).attr('id') + '-info').hide();
+	(function ($) {
+	
+	  Drupal.behaviors.patternsBehavior = {
+	    attach: function(context, settings) {
+		  $('.pattern-info').hide();
+		  $('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
+		  $('.pattern-title').hover(
+		    function(){
+		      $('#' + $(this).attr('id') + '-info').show();
+		    },
+		    function(){
+		      $('#' + $(this).attr('id') + '-info').hide();
+		    }
+		  );
 	    }
-	  );
-    }
-  };
-
-})(jQuery);
-
-
-
-//Drupal.behaviors.patternsBehavior = function (context) {
-//  $('.pattern-info').hide();
-//  $('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
-//  $('.pattern-title').hover(
-//    function(){
-//      $('#' + $(this).attr('id') + '-info').show();
-//    },
-//    function(){
-//      $('#' + $(this).attr('id') + '-info').hide();
-//    }
-//  );
-//};
+	  };
+	})(jQuery);
+	
+  	var url = location.href.substring(0,location.href.lastIndexOf('admin/patterns/') + 'admin/patterns/'.length) + 'refresh';
+	
+	var refresh = document.getElementById("refresh-all");
+	
+	if (refresh) {	
+		refresh.onclick = function() {
+			document.getElementById('all_patterns_div').innerHTML = '...refreshing...';
+			
+			jQuery.ajax({
+				type: 'POST',
+				url: url,
+				// TODO: This needs to be encoded.
+				//data: 'pattern=' + textarea.value,
+				success: function ( data, status, xhr ) {
+							document.getElementById('all_patterns_div').innerHTML = data;
+							//$('all_patterns_div').replaceWith(data);
+						}
+			});
+		};
+	}
+	else{
+		alert('NO');
+	}
+}
