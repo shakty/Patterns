95e1f8b0a603eabcaf7d082eb4c9ffb73cca16a5
Stefano Balietti <sbalietti@ethz.ch>
2011-10-04 12:47:30 +0200

    Slightly Modified patterns.js

--- js/patterns.js
+++ js/patterns.js
@@ -1,36 +1,41 @@
 window.onload = function() {
 	
-	jQuery('.pattern-info').hide();
-	jQuery('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
-	jQuery('.pattern-title').hover(
-	    function(){
-	    	//jQuery('.pattern-info').show();
-	    	//alert('#' + jQuery(this).attr('id') + '-info');
-	    	jQuery('#' + jQuery(this).attr('id') + '-info').show();
-	    },
-	    function(){
-	    	jQuery('#' + jQuery(this).attr('id') + '-info').hide();
-	    }
-	  );
+	(function ($) {
+		  
+		jQuery('.pattern-info').hide();
+		jQuery('.pattern-info').css({'position' : 'absolute', 'width' : '300px'});
+		jQuery('.pattern-title').hover(
+		    function(){
+		    	//jQuery('.pattern-info').show();
+		    	//alert('#' + jQuery(this).attr('id') + '-info');
+		    	jQuery('#' + jQuery(this).attr('id') + '-info').show();
+		    },
+		    function(){
+		    	jQuery('#' + jQuery(this).attr('id') + '-info').hide();
+		    }
+		  );
+		
+	  	var url = location.href.substring(0,location.href.lastIndexOf('admin/patterns') + 'admin/patterns'.length) + '/refresh';
+		alert(url);
+		var refresh = document.getElementById("refresh-all");
+		
+		if (refresh) {	
+			refresh.onclick = function() {
+				document.getElementById('all_patterns_div').innerHTML = '...refreshing...';
+				
+				jQuery.ajax({
+					type: 'POST',
+					url: url,
+					// TODO: This needs to be encoded.
+					//data: 'pattern=' + textarea.value,
+					success: function ( data, status, xhr ) {
+								document.getElementById('all_patterns_div').innerHTML = data;
+								//$('all_patterns_div').replaceWith(data);
+							}
+				});
+			};
+		}
 	
-  	var url = location.href.substring(0,location.href.lastIndexOf('admin/patterns/') + 'admin/patterns/'.length) + 'refresh';
 	
-	var refresh = document.getElementById("refresh-all");
-	
-	if (refresh) {	
-		refresh.onclick = function() {
-			document.getElementById('all_patterns_div').innerHTML = '...refreshing...';
-			
-			jQuery.ajax({
-				type: 'POST',
-				url: url,
-				// TODO: This needs to be encoded.
-				//data: 'pattern=' + textarea.value,
-				success: function ( data, status, xhr ) {
-							document.getElementById('all_patterns_div').innerHTML = data;
-							//$('all_patterns_div').replaceWith(data);
-						}
-			});
-		};
-	}
+	})(jQuery);
 }
